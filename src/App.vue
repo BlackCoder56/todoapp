e0c45a71180561630338809197f0d5b4b9bdb8aa
BlackCoder56 <blackcoda56@gmail.com>
2025-02-23 23:07:06 +0300

    list and form of all

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,47 @@
 <script setup>
-</script>
+    import { ref } from 'vue';
 
+    const tasks = ref(['Orange', 'Mango', 'Apple']);
+
+</script>
 <template>
-  <h1>My Todo App</h1>
+    <h1>TODO Application</h1>
+
+    <form @submit.prevent="">
+        <input type="text" name="newTask" placeholder="Enter new Task" />
+        <button type="submit">Submit</button>
+    </form>
+    <br>
+    <table border="1">
+     <tr>
+      <!-- <th>No.</th> -->
+        <th>Task name</th>
+        <th>Status</th>
+        <th>Actions</th>
+     </tr>
+     <tr v-for="(task, index) in tasks">
+        <!-- <td>{{ index }}</td> -->
+        <td>{{ task }}</td>
+     </tr>
+    </table>
 </template>
+
+<style scoped>
+  h1{
+    text-decoration: underline;
+  }
+  input{
+    font-size: 20px;
+    margin-right: 10px;
+  }
+
+  button{
+    font-size: 20px;
+    color:cadetblue;
+  }
+
+  table{
+    border-collapse: collapse;
+  }
+
+</style>
